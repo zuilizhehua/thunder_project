a0f191d2ce3686b73a4216c673cc5b63b9845764
zhaichunlei <zhaichunlei@jikugroup.com>
2023-04-04 17:11:46 +0800

    master

---
+++ src/main/java/com/zuilizhehua/thunder_project/ThunderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zuilizhehua.thunder_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThunderProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zuilizhehua/thunder_project/ThunderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuilizhehua.thunder_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
