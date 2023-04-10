913406035f273b1e2f7201da8cc6485a2f0f676f
zhaichunlei <zhaichunlei@jikugroup.com>
2023-04-10 21:35:07 +0800

    login

---
+++ src/main/java/com/zuilizhehua/user/config/ShiroConfig.java
@@ -0,0 +1,54 @@
+package com.zuilizhehua.user.config;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    /**
+     * shiroFilter 过滤
+     */
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        //给shiroFilter配置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        //配置系统受限资源
+        //配置系统公共资源
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("/user/login","anon");
+        //表示这个为公共资源 一定是在受限资源上面
+        map.put("/**","authc");
+        //表示这个受限资源需要认证和授权
+        // 设置认证界面路径
+        shiroFilterFactoryBean.setLoginUrl("/login.jsp");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
+
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * 创建安全管理器
+     */
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm")UserRealm userRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+
+    @Bean(name = "userRealm")
+    public UserRealm userRealm(){
+        return new UserRealm();
+    }
+
+}
