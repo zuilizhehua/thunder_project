e9d11867c803547d24f2e924f8eb2179a3e291b3
zhaichunlei <zhaichunlei@jikugroup.com>
2023-04-18 20:17:05 +0800

    login

--- src/main/java/com/zuilizhehua/user/config/UserRealm.java
+++ src/main/java/com/zuilizhehua/user/config/UserRealm.java
@@ -3,6 +3,7 @@ package com.zuilizhehua.user.config;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
@@ -19,6 +20,10 @@ public class UserRealm extends AuthorizingRealm {
     //认证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String principal = (String) authenticationToken.getPrincipal();
+        if ("zhaichunlei".equals(principal)) {
+            return new SimpleAuthenticationInfo(principal,"123",this.getName());
+        }
         return null;
     }
 }
--- src/main/java/com/zuilizhehua/user/controller/LoginController.java
+++ src/main/java/com/zuilizhehua/user/controller/LoginController.java
@@ -1,15 +1,34 @@
 package com.zuilizhehua.user.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "bs/user")
+@Controller
+@RequestMapping(value = "/user")
 public class LoginController {
 
     @PostMapping("login")
-    public void login(){
+    public String login(String username, String password){
         System.out.println("登录了");
+        //获取主题对象
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(new UsernamePasswordToken(username,password));
+            System.out.println("登录成功！！！");
+            return "redirect:/index.jsp";
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            System.out.println("用户错误！！！");
+        } catch (IncorrectCredentialsException e) {
+            System.out.println("密码错误！！！");
+        }
+        return "redirect:/login.jsp";
     }
 }
